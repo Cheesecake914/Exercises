ab3017985a04188fc57746ae1a62dba26768d0ea
Cheesecake914 <86504648+Cheesecake914@users.noreply.github.com>
2021-08-10 15:54:20 +0800

    20210810 - case and switch exercise

---
+++ using switch and case.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    char grade;
+    printf("Enter your grade (A-F): ");
+    scanf("%c", &grade);
+    switch(grade)
+    {
+    case 'A':
+        printf("Grade between 90-100 \n");
+        break;
+    case 'B':
+        printf("Grade between 80-89 \n");
+        break;
+    case 'C':
+        printf("Grade between 70-79 \n");
+        break;
+    case 'D':
+        printf("Grade between 60-69 \n");
+        break;
+    case 'F':
+        printf("Grade lower than 60 \n");
+        break;
+    default:
+        printf("Error... \n");
+        break;
+    }
+}
